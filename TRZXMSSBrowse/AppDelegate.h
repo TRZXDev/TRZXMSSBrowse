85b2a48efd9cbf43097ee5cbda5858426c946bff
zhangjw@mmwipo.com <zhangjw@mmwipo.com>
2017-03-01 14:17:57 +0800

    Initial Commit

---
+++ TRZXMSSBrowse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TRZXMSSBrowse
+//
+//  Created by 张江威 on 2017/3/1.
+//  Copyright © 2017年 张江威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TRZXMSSBrowse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TRZXMSSBrowse
+//
+//  Created by 张江威 on 2017/3/1.
+//  Copyright © 2017年 张江威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
